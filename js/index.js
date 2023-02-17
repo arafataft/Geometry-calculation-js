f4ef72292f9824dc4d8f7ff26c366b785a05790e
arafataft <arafatcd@gmail.com>
2023-02-18 05:50:23 +0600

    funtion add and called

--- js/index.js
+++ js/index.js
@@ -8,31 +8,58 @@ document.getElementById("btn-blog").addEventListener('click', function () {
 
 // card 
 let serial=0;
-document.getElementById("first-card").addEventListener("click", function () {
-    serial += 1;
+// common function 
 
-    const firstValue=document.getElementById("triangleFirstInput").value;
-    const secondValue=document.getElementById("triangleSecondInput").value;
-    const name=document.getElementById("first-name").innerText;
-
+function getValue(firstValue,secondValue,name){
+    const firsValue=document.getElementById(firstValue).value;
+    const sValue=document.getElementById(secondValue).value;
+    const Nname=document.getElementById(name).innerText;
 
     if (
-        firstValue == "" ||
-        secondValue == "" ||
-        firstValue < 0 ||
-        secondValue < 0 || 
-        isNaN(firstValue)||
-        isNaN(secondValue)
+        firsValue == "" ||
+        sValue == "" ||
+        firsValue < 0 ||
+        sValue < 0 || 
+        isNaN(firsValue)||
+        isNaN(sValue)
       ) {
         return alert("please enter any valid number");
       }
-    const area=0.5*parseFloat(firstValue)*parseFloat(secondValue);
-    // const area=0.5*areav;
 
-    console.log(area);
-    console.log(firstValue);
-    console.log(secondValue);
-    // display
+    const nameValue={
+        firstValue: firsValue,
+        secondValue: sValue,
+        name: Nname,
+    };
+    return nameValue;
+}
+
+function getInnerInfo(firstValue,secondValue,name){
+    const firsValue=document.getElementById(firstValue).innerText;
+    const sValue=document.getElementById(secondValue).innerText;
+    const Nname=document.getElementById(name).innerText;
+
+    if (
+        firsValue == "" ||
+        sValue == "" ||
+        firsValue < 0 ||
+        sValue < 0 || 
+        isNaN(firsValue)||
+        isNaN(sValue)
+      ) {
+        return alert("please enter any valid number");
+      }
+
+    const nameValue={
+        firstValue: firsValue,
+        secondValue: sValue,
+        name: Nname,
+    };
+    return nameValue;
+}
+
+
+function display(serial,name,area){
     const container = document.getElementById("table-container");
     const tr = document.createElement("tr");
     tr.innerHTML = `
@@ -40,10 +67,48 @@ document.getElementById("first-card").addEventListener("click", function () {
     <td>${name}</td>
     <td>${area+'cm<sup>2</sup>'}</td>
     <td>
-    <button class="btn btn-primary ">covert to<span class="lowercase">m<sup>2</sup></span> </button>
+    <button class="btn btn-primary ">covert to <span class="lowercase">m<sup>2</sup></span> </button>
     </td>
     
   `;
     container.appendChild(tr);
     document.getElementById("area-calculation").innerText = serial;
+}
+
+// 1st card 
+document.getElementById("first-card").addEventListener("click", function () {
+    serial += 1;
+    
+    const nameValue=getValue("triangleFirstInput","triangleSecondInput","first-name");
+    
+    const area=0.5*parseFloat(nameValue.firstValue)*parseFloat(nameValue.secondValue);
+
+    display(serial,nameValue.name,area);
+
+});
+
+// 2nd card 
+document.getElementById("second-card").addEventListener("click", function () {
+    serial += 1;
+    
+    const nameValue=getValue("rectangleFirstInput","rectangleSecondInput","second-name");
+    
+    const area=parseFloat(nameValue.firstValue)*parseFloat(nameValue.secondValue);
+
+    display(serial,nameValue.name,area);
+
+});
+
+
+// 3rd card 
+
+document.getElementById("third-card").addEventListener("click", function () {
+    serial += 1;
+    
+    const nameValue=getInnerInfo("parallelogramFirstValue","parallelogramSecondValue","third-name");
+    
+    const area=0.5*parseFloat(nameValue.firstValue)*parseFloat(nameValue.secondValue);
+
+    display(serial,nameValue.name,area);
+
 });
