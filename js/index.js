0128a0dceaf3d37903223e3ba79fdb7f263f5aac
arafataft <arafatcd@gmail.com>
2023-02-18 22:11:26 +0600

    all card function add

--- js/index.js
+++ js/index.js
@@ -112,3 +112,60 @@ document.getElementById("third-card").addEventListener("click", function () {
     display(serial,nameValue.name,area);
 
 });
+
+// 4rd card 
+
+document.getElementById("four-card").addEventListener("click", function () {
+    serial += 1;
+    
+    const nameValue=getInnerInfo("rhombusFirstValue","rhombusSecondValue","four-name");
+    
+    const area=0.5*parseFloat(nameValue.firstValue)*parseFloat(nameValue.secondValue);
+
+    display(serial,nameValue.name,area);
+
+});
+// 5rd card 
+
+document.getElementById("five-card").addEventListener("click", function () {
+    serial += 1;
+    
+    const nameValue=getInnerInfo("pentagonFirstValue","pentagonSecondValue","five-name");
+    
+    const area=3.1416*parseFloat(nameValue.firstValue)*parseFloat(nameValue.secondValue);
+
+    display(serial,nameValue.name,area);
+
+});
+
+
+// last card 
+
+document.getElementById("last-card").addEventListener("click", function () {
+    serial += 1;
+    
+    const nameValue=getInnerInfo("ellipseFirstValue","ellipseSecondValue","last-name");
+    
+    const area=0.5*parseFloat(nameValue.firstValue)*parseFloat(nameValue.secondValue);
+
+    display(serial,nameValue.name,area);
+
+});
+
+
+
+
+
+
+// randam background color 
+const cards = document.querySelectorAll(".bg-color-random");
+for (const card of cards) {
+  const originalColor = card.style.backgroundColor;
+  card.addEventListener("mouseover", () => {
+    const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    card.style.backgroundColor = randomColor;
+  });
+  card.addEventListener("mouseout", () => {
+    card.style.backgroundColor = originalColor;
+  });
+}
